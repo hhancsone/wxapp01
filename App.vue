3693c8d8edf0321cc108069b53c5115d72151dd3
hwd <703850016@qq.com>
2026-03-03 15:45:50 +0800

    Upload file: App.vue

---
+++ App.vue
@@ -0,0 +1,29 @@
+<script>
+	import uniIdPageInit from '@/uni_modules/uni-id-pages/init.js';
+	export default {
+		onLaunch: async function() {
+			console.log('App Launch')
+			await uniIdPageInit()
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/* 每个页面公共css */
+	page {
+		background-color: #f8f8f8;
+		font-size: 28rpx;
+		color: #333;
+	}
+
+	view,
+	text {
+		box-sizing: border-box;
+	}
+</style>
